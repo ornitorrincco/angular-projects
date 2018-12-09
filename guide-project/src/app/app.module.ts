8f5ca08e64e31883c507aa220a0f47cef64c034b
ornitorrincco <ornitorrincco@gmail.com>
2018-12-08 16:29:31 -0800

    course in progress

--- guide-project/src/app/app.module.ts
+++ guide-project/src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoursesComponent } from './courses.component';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- guide-project/src/app/courses.component.ts
+++ guide-project/src/app/courses.component.ts
@@ -1,8 +1,25 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 // Decorator function
+// modify dom *ngFor
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  template: `
+    <h2>{{title}}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+    <img src="{{ imgUrl }}" />
+    `
 })
 export class CoursesComponent {
+  title = "List of courses";
+  imgUrl = "http://lorempixel.com/g/800/400/";
+  courses;
+  constructor(service: CoursesService) {
+    // let service = new CoursesService();
+    this.courses = service.getCourses();
+  }
 }
